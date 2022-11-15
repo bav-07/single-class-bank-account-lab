1f7473a6a8ead45ce53b060c2359afbe8fbe545a
Bavaharsan Nagarajah <bavaharsan.nagarajah@gmail.com>
2022-11-15 17:33:50 +0000

    first commit

---
+++ src/main/java/BankAccount.java
@@ -0,0 +1,69 @@
+import java.time.LocalDate;
+
+public class BankAccount {
+
+    // PROPERTIES
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private int accountNumber;
+    private double balance = 0;
+
+    // CONSTRUCTOR
+    public BankAccount(String firstName, String lastName, LocalDate dateOfBirth, int accountNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.accountNumber  = accountNumber;
+    }
+
+    // METHODS
+    public String deposit(double depositAmount) {
+        if (depositAmount <= 0) {
+            return "Error: Cannot deposit negative quantities.";
+        }
+        else {
+            this.balance += depositAmount;
+            return "Success. Your account contains £" + this.balance;
+        }
+    }
+
+    // GETTERS & SETTERS
+    public String getFirstName() {
+        return this.firstName;
+    }
+    public void setFirstName(String newFirstName) {
+        this.firstName = newFirstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+    public void setLastName(String newLastName) {
+        this.lastName = newLastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return this.dateOfBirth;
+    }
+    public void setDateOfBirth(LocalDate newDateOfBirth) {
+        this.dateOfBirth = newDateOfBirth;
+    }
+
+    public int getAccountNumber() {
+        return this.accountNumber;
+    }
+    public void setAccountNumber(int newAccountNumber) {
+        this.accountNumber = newAccountNumber;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+    public void setBalance(double newBalance) {
+        this.balance = newBalance;
+    }
+
+
+
+}
---
+++ src/test/java/BankAccountTest.java
@@ -0,0 +1,104 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class BankAccountTest {
+
+    private BankAccount account;
+
+    @BeforeEach
+    public void setUp() {
+        account = new BankAccount(
+                "Bav",
+                "Nagarajah",
+                LocalDate.of(2000,1,25),
+                12345678);
+        account.setBalance(0);
+    }
+
+    @Test
+    public void hasFirstName() {
+        assertThat(account.getFirstName()).isEqualTo("Bav");
+    }
+
+    @Test
+    public void canSetFirstName() {
+        account.setFirstName("John");
+        assertThat(account.getFirstName()).isEqualTo("John");
+    }
+
+    @Test
+    public void hasLastName() {
+        assertThat(account.getLastName()).isEqualTo("Nagarajah");
+    }
+
+    @Test
+    public void canSetLastName() {
+        account.setLastName("Smith");
+        assertThat(account.getLastName()).isEqualTo("Smith");
+    }
+
+    @Test
+    public void hasDateOfBirth() {
+        int actualDayOfMonth = account.getDateOfBirth().getDayOfMonth();
+        int actualMonth = account.getDateOfBirth().getMonthValue();
+        int actualYear = account.getDateOfBirth().getYear();
+        assertThat(actualDayOfMonth).isEqualTo(25);
+        assertThat(actualMonth).isEqualTo(1);
+        assertThat(actualYear).isEqualTo(2000);
+    }
+
+    @Test
+    public void canSetDateOfBirth() {
+        account.setDateOfBirth(LocalDate.of(1850,5,31));
+        int actualDayOfMonth = account.getDateOfBirth().getDayOfMonth();
+        int actualMonth = account.getDateOfBirth().getMonthValue();
+        int actualYear = account.getDateOfBirth().getYear();
+        assertThat(actualDayOfMonth).isEqualTo(31);
+        assertThat(actualMonth).isEqualTo(5);
+        assertThat(actualYear).isEqualTo(1850);
+    }
+
+    @Test
+    public void hasAccountNumber() {
+        assertThat(account.getAccountNumber()).isEqualTo(12345678);
+    }
+
+    @Test
+    public void canSetAccountNumber() {
+        account.setAccountNumber(87654321);
+        assertThat(account.getAccountNumber()).isEqualTo(87654321);
+    }
+
+    @Test
+    public void hasBalance() {
+        assertThat(account.getBalance()).isEqualTo(0);
+    }
+
+    @Test
+    public void canSetBalance() {
+        account.setBalance(1000);
+        assertThat(account.getBalance()).isEqualTo(1000);
+        // System.out.println(account.getBalance());
+        account.setBalance(1560.34);
+        assertThat(account.getBalance()).isEqualTo(1560.34);
+        // System.out.println(account.getBalance());
+        account.setBalance(578423942394.34);
+        assertThat(account.getBalance()).isEqualTo(578423942394.34);
+    }
+
+    @Test
+    public void canDepositAmount() {
+        assertThat(account.deposit(1000.0000)).isEqualTo("Success. Your account contains £1000.0");
+        assertThat(account.getBalance()).isEqualTo(1000);
+        assertThat(account.deposit(240.54)).isEqualTo("Success. Your account contains £1240.54");
+        assertThat(account.getBalance()).isEqualTo(1240.54);
+        assertThat(account.deposit(-245)).isEqualTo("Error: Cannot deposit negative quantities.");
+    }
+
+
+
+}
